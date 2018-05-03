940d4b9d601a08d1089f956a21c202ed2bbd5a5c
Andrey <a.imshenick@gmail.com>
2018-05-03 18:05:10 +0300

    major: editing DAO Student class and test

--- src/main/java/net/imshenik/university/dao/postgres/DAOStudent.java
+++ src/main/java/net/imshenik/university/dao/postgres/DAOStudent.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 import org.apache.log4j.Logger;
@@ -13,46 +12,127 @@ import net.imshenik.university.domain.entities.Student;
 public class DAOStudent {
     private static Logger logger = Logger.getLogger(DAOStudent.class.getName());
     
-    public Set<Student> getAllStudents() throws DAOException {
+    public Set<Student> findAll() throws DAOException {
         logger.trace("Getting list of all students:");
         Set<Student> students = new HashSet<Student>();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
+        String sql = "select * FROM students;";
         try {
             Class.forName("org.postgresql.Driver");
-            logger.trace("Opening connection...");
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university", "andrey",
-                    "1234321");
-            String sql = "select * FROM students;";
-            statement = connection.prepareStatement(sql);
-            logger.trace("Executing SQL statement...");
-            resultSet = statement.executeQuery();
-            logger.trace("Data retrieved, going to iterate...");
-            while (resultSet.next()) {
-                Student student = new Student();
-                student.setId(resultSet.getInt("id"));
-                student.setFirstName((resultSet.getString("firstname")));
-                student.setLastName((resultSet.getString("lastname")));
-                students.add(student);
+            logger.trace("Trying to create Connection, PreparedStatement and ResultSet...");
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university",
+                    "andrey", "1234321");
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    ResultSet resultSet = statement.executeQuery();) {
+                while (resultSet.next()) {
+                    Student student = new Student();
+                    student.setId(resultSet.getInt("id"));
+                    student.setFirstName((resultSet.getString("firstname")));
+                    student.setLastName((resultSet.getString("lastname")));
+                    students.add(student);
+                }
+            } catch (Exception e) {
+                logger.error("Unable to read all users from database", e);
+                throw new DAOException("Unable to read all users from database", e);
             }
-            logger.trace("ResultSet provided " + students.size() + "entities");
-        } catch (Exception e) {
-            throw new DAOException("Unable to read all users from database");
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (statement != null) {
-                    statement.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Unable to load driver org.postgresql.Driver", e);
+            throw new DAOException("Unable to load driver org.postgresql.Driver", e);
         }
         return students;
+    }
+    
+    public Student findOne(int id) throws DAOException {
+        logger.trace("Finding Student by ID:");
+        String sql = "select * from students where id=?;";
+        try {
+            Class.forName("org.postgresql.Driver");
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university",
+                    "andrey", "1234321"); PreparedStatement statement = connection.prepareStatement(sql);) {
+                statement.setInt(1, id);
+                try (ResultSet resultSet = statement.executeQuery();) {
+                    resultSet.next();
+                    Student student = new Student();
+                    student.setId(resultSet.getInt("id"));
+                    student.setFirstName((resultSet.getString("firstname")));
+                    student.setLastName((resultSet.getString("lastname")));
+                    return student;
+                } catch (Exception e) {
+                    logger.error("Unable to create ResultSet", e);
+                    throw new DAOException("Unable to create ResultSet", e);
+                }
+            } catch (Exception e) {
+                logger.error("Unable to create Connection", e);
+                throw new DAOException("Unable to create Connection", e);
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Unable to load driver org.postgresql.Driver", e);
+            throw new DAOException("Unable to load driver org.postgresql.Driver", e);
+        }
+    }
+    
+    public boolean create(String firstName, String lastName) throws DAOException {
+        logger.trace("Creating new Student:");
+        String sql = "insert into students (firstname,lastname) values (?,?);";
+        try {
+            Class.forName("org.postgresql.Driver");
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university",
+                    "andrey", "1234321"); PreparedStatement statement = connection.prepareStatement(sql);) {
+                statement.setString(1, firstName);
+                statement.setString(2, lastName);
+                int rowsInserted = statement.executeUpdate();
+                logger.trace("Inserted " + rowsInserted + " row(s)");
+                return rowsInserted > 0 ? true : false;
+            } catch (Exception e) {
+                logger.error("Unable to open connection", e);
+                throw new DAOException("Unable to open connection", e);
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Unable to load driver org.postgresql.Driver", e);
+            throw new DAOException("Unable to load driver org.postgresql.Driver", e);
+        }
+    }
+    
+    public boolean update(int id, String firstName, String lastName) throws DAOException {
+        logger.trace("Updating Student:");
+        String sql = "update students set firstname=?,lastname=? where id=?;";
+        try {
+            Class.forName("org.postgresql.Driver");
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university",
+                    "andrey", "1234321"); PreparedStatement statement = connection.prepareStatement(sql);) {
+                statement.setString(1, firstName);
+                statement.setString(2, lastName);
+                statement.setInt(3, id);
+                int rowsUpdated = statement.executeUpdate();
+                logger.trace("Updated " + rowsUpdated + " row(s)");
+                return rowsUpdated > 0 ? true : false;
+            } catch (Exception e) {
+                logger.error("Unable to open connection", e);
+                throw new DAOException("Unable to open connection", e);
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Unable to load driver org.postgresql.Driver", e);
+            throw new DAOException("Unable to load driver org.postgresql.Driver", e);
+        }
+    }
+    
+    public boolean delete(int id) throws DAOException {
+        logger.trace("Updating Student:");
+        String sql = "delete from students as s where s.id = ?;";
+        try {
+            Class.forName("org.postgresql.Driver");
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/university",
+                    "andrey", "1234321"); PreparedStatement statement = connection.prepareStatement(sql);) {
+                statement.setInt(1, id);
+                int rowsDeleted = statement.executeUpdate();
+                logger.trace("Deleted " + rowsDeleted + " row(s)");
+                return rowsDeleted > 0 ? true : false;
+            } catch (Exception e) {
+                logger.error("Unable to open connection", e);
+                throw new DAOException("Unable to open connection", e);
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Unable to load driver org.postgresql.Driver", e);
+            throw new DAOException("Unable to load driver org.postgresql.Driver", e);
+        }
     }
 }
--- src/test/java/net/imshenik/university/TestDAOStudent.java
+++ src/test/java/net/imshenik/university/TestDAOStudent.java
@@ -1,5 +1,6 @@
 package net.imshenik.university;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import java.util.Set;
 import org.junit.Test;
@@ -7,14 +8,38 @@ import net.imshenik.university.dao.postgres.DAOException;
 import net.imshenik.university.dao.postgres.DAOStudent;
 import net.imshenik.university.domain.entities.Student;
 
-
 public class TestDAOStudent {
+    DAOStudent daoStudent = new DAOStudent();
     
     @Test
-    public void getAllStudentsTest() throws DAOException {
-        DAOStudent daoStudent = new DAOStudent();
+    public void findAllTest() throws DAOException {
         Set<Student> students = null;
-        students = daoStudent.getAllStudents();
+        students = daoStudent.findAll();
         assertNotNull(students);
+    }
+    
+    @Test
+    public void findOneTest() throws DAOException {
+        Student expected = daoStudent.findOne(1);
+        Student actual = new Student();
+        actual.setId(1);
+        actual.setFirstName("Andrey");
+        actual.setLastName("Imshenik");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void createTest() throws DAOException {
+        boolean expected = true; 
+        boolean actual = daoStudent.create("Nickota", "Ivanov");
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void updateTest() {
+    }
+    
+    @Test
+    public void deleteTest() {
     }
 }
