409f9f9ffc31b742cd6644adcc5227ba8a3794a3
Andrey <a.imshenick@gmail.com>
2018-05-18 14:22:08 +0300

    task10 v5 interface ClassroomDao : removed dublicated methods

--- src/main/java/net/imshenik/university/dao/ClassroomDao.java
+++ src/main/java/net/imshenik/university/dao/ClassroomDao.java
@@ -1,16 +1,5 @@
 package net.imshenik.university.dao;
 
-import java.util.List;
 
 public interface ClassroomDao<T> extends CommonDao<T>{
-
-    public List<T> findAll() throws DaoException;
-
-    public T findOne(int id) throws DaoException;
-
-    public T create(T t) throws DaoException;
-
-    public void update(T t) throws DaoException;
-
-    public void delete(int id) throws DaoException;
 }
