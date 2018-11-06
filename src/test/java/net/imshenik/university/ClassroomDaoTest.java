6a7447e450807f23de186bac6751a1dc3a627e68
aimshenik <a.imshenick@gmail.com>
2018-11-06 23:48:04 +0300

    embeded H2handler in test

--- src/test/java/net/imshenik/university/ClassroomDaoTest.java
+++ src/test/java/net/imshenik/university/ClassroomDaoTest.java
@@ -14,76 +14,15 @@ import net.imshenik.university.dao.DaoException;
 import net.imshenik.university.domain.Classroom;
 
 public class ClassroomDaoTest {
-    ClassroomDaoPostgres classroomDaoPostgres = null;
-    
-    public ClassroomDaoTest() throws DaoException {
-        classroomDaoPostgres = new ClassroomDaoPostgres();
-    }
-    
-    // @Test
-    // public void findAllTest() throws DaoException {
-    // List<Classroom> classrooms = null;
-    // classrooms = classroomDaoPostgres.findAll();
-    // assertTrue(classrooms.size() > 0);
-    // }
-    //
-    // @Test
-    // public void findOneTest() throws DaoException {
-    // Classroom classroom = classroomDaoPostgres.findOne(Integer.MAX_VALUE);
-    // assertNull(classroom);
-    // Classroom classroom2 = classroomDaoPostgres.findOne(1);
-    // assertNotNull(classroom2);
-    // }
-    //
-    // @Test
-    // public void createTest() throws DaoException {
-    // Classroom classroom = classroomDaoPostgres.create(new Classroom(0, "213",
-    // "26a", 50));
-    // assertTrue(classroom.getId() != 0);
-    // }
-    //
-    // @Test
-    // public void updateTest() throws DaoException {
-    // Classroom classroom = classroomDaoPostgres.create(new Classroom(0, null,
-    // null, 0));
-    // classroom.setBuilding("67b");
-    // classroomDaoPostgres.update(classroom);
-    // }
-    //
-    // @Test
-    // public void deleteTest() throws DaoException {
-    // Classroom newClassroom = classroomDaoPostgres.create(new Classroom(0, "TO
-    // DELETE", "TO DELETE", 5000));
-    // int lastID = newClassroom.getId();
-    // classroomDaoPostgres.delete(lastID);
-    // Classroom removedClassroom = classroomDaoPostgres.findOne(lastID);
-    // assertNull(removedClassroom);
-    // }
-    @Test
-    public void classroomCrudTest() throws DaoException {
-    	try {
-			System.out.println("Current DIR is :" + new java.io.File( "." ).getAbsolutePath());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        Classroom firstClassroom = new Classroom("22", "d", 50);
-        Classroom secondClassroom = new Classroom("23", "d", 10);
-        classroomDaoPostgres.create(firstClassroom);
-        classroomDaoPostgres.create(secondClassroom);
-        assertTrue(firstClassroom.getId() != 0);
-        assertTrue(secondClassroom.getId() != 0);
-        List<Classroom> classrooms = classroomDaoPostgres.findAll();
-        assertTrue(classrooms.size() >= 2);
-        Classroom foundClassroom = classroomDaoPostgres.findOne(firstClassroom.getId());
-        assertEquals(foundClassroom, firstClassroom);
-        secondClassroom.setBuilding("A");
-        classroomDaoPostgres.update(secondClassroom);
-        Classroom updatedClassroom = classroomDaoPostgres.findOne(secondClassroom.getId());
-        assertEquals(updatedClassroom, secondClassroom);
-        classroomDaoPostgres.delete(firstClassroom.getId());
-        classroomDaoPostgres.delete(secondClassroom.getId());
-        assertNull(classroomDaoPostgres.findOne(firstClassroom.getId()));
-        assertNull(classroomDaoPostgres.findOne(secondClassroom.getId()));
-    }
+	ClassroomDaoPostgres classroomDaoPostgres = new ClassroomDaoPostgres();
+
+	@Test
+	public void classroomCrudTest() throws DaoException {
+		H2handler.createTable("H2ClasstoomsCreation.sql");
+		Classroom classroom = new Classroom("22", "d", 50);
+		classroomDaoPostgres.create(classroom);
+		List<Classroom> classrooms = classroomDaoPostgres.findAll();
+		classroomDaoPostgres.findOne(classroom.getId());
+		classroomDaoPostgres.delete(classroom.getId());
+	}
 }
--- src/test/java/net/imshenik/university/H2handler.java
+++ src/test/java/net/imshenik/university/H2handler.java
@@ -8,24 +8,24 @@ import org.junit.Test;
 import net.imshenik.university.dao.ConnectionFactory;
 
 public class H2handler {
-    @Test
-    public static void main(String[] args) {
-        String sql = "";
-        try (FileReader fileReader = new FileReader(new File(
-                "C:\\Users\\u0173307\\eclipse-workspace\\Task10DAOLayer\\src\\test\\resources\\sql\\h2tablecreation.sql"))) {
-            int i = fileReader.read();
-            while (i != -1) {
-                sql += (char) i;
-                System.out.print((char) i);
-                i = fileReader.read();
-            }
-            Class.forName("org.h2.Driver");
-            try (Connection connection = ConnectionFactory.getConnection();
-                    Statement stmt = connection.createStatement()) {
-                stmt.execute(sql);
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	@Test
+	public static void createTable(String source) {
+		String sql = "";
+		try (FileReader fileReader = new FileReader(
+				new File(H2handler.class.getClassLoader().getResource("sql/" + source).getFile()))) {
+			int i = fileReader.read();
+			while (i != -1) {
+				sql += (char) i;
+				System.out.print((char) i);
+				i = fileReader.read();
+			}
+			Class.forName("org.h2.Driver");
+			try (Connection connection = ConnectionFactory.getConnection();
+					Statement stmt = connection.createStatement()) {
+				stmt.execute(sql);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
