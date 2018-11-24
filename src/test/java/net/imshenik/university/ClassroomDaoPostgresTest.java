8cdc9bdd4ce68f2e44cc3ac6dc89149efbed880a
aimshenik <a.imshenick@gmail.com>
2018-11-24 22:57:36 +0300

    fixed all comments from 13 merge request

--- src/test/java/net/imshenik/university/ClassroomDaoPostgresTest.java
+++ src/test/java/net/imshenik/university/ClassroomDaoPostgresTest.java
@@ -4,114 +4,102 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import net.imshenik.university.dao.ClassroomDaoPostgres;
-import net.imshenik.university.dao.ConnectionFactory;
 import net.imshenik.university.dao.DaoException;
 import net.imshenik.university.domain.Classroom;
 
 public class ClassroomDaoPostgresTest {
-	private ClassroomDaoPostgres classroomDaoPostgres = new ClassroomDaoPostgres();
-	private H2handler h2handler = H2handler.getInstance();
-	private final static String tablename = "CLASSROOMS";
+    private ClassroomDaoPostgres classroomDaoPostgres = new ClassroomDaoPostgres();
+    private H2handler h2handler = H2handler.getInstance();
+    private final static String TABLENAME = "CLASSROOMS";
+    private final static String NUMBER = "NUMBER";
+    private final static String BUILDING = "BUILDING";
+    private final static String CAPACITY = "CAPACITY";
+    private final static String SQL_CREATE_FILENAME = "H2ClassroomsDropCreate.sql";
+    private final static String SQL_INSERT_FILENAME = "H2ClassroomsInsert.sql";
 
-	@Test
-	public void classroomCreateTest() throws Exception {
-		Classroom expected = new Classroom("22", "d", 50);
-		int before = H2handler.getNumberOfRows(tablename);
-		Classroom actual = classroomDaoPostgres.create(expected);
-		int after = H2handler.getNumberOfRows(tablename);
-		assertEquals(after - before, 1);
-		assertTrue(compare(expected, actual));
-	}
+    @Test
+    public void classroomCreateTest() throws Exception {
+        Classroom expected = new Classroom("22", "d", 50);
+        int before = H2handler.getNumberOfRows(TABLENAME);
+        Classroom actual = classroomDaoPostgres.create(expected);
+        int after = H2handler.getNumberOfRows(TABLENAME);
+        assertEquals(after - before, 1);
+        assertTrue(equals(expected, actual));
+    }
 
-	@Test
-	public void classroomFindOneTest() throws Exception {
-		Classroom[] etalon = { new Classroom(1, "106", "23", 30), new Classroom(2, "211", "23", 50),
-				new Classroom(3, "321", "23", 30), new Classroom(4, "401", "24", 100),
-				new Classroom(5, "305", "24", 30) };
-		for (int i = 0; i < etalon.length; i++) {
-			Classroom expected = etalon[i];
-			Classroom actual = classroomDaoPostgres.findOne(i + 1);
-			assertTrue(compare(expected, actual));
-		}
-	}
+    @Test
+    public void classroomFindOneTest() throws Exception {
+        Classroom[] classrooms = { new Classroom(1, "106", "23", 30), new Classroom(2, "211", "23", 50),
+                new Classroom(3, "321", "23", 30), new Classroom(4, "401", "24", 100),
+                new Classroom(5, "305", "24", 30) };
+        for (int i = 0; i < classrooms.length; i++) {
+            Classroom expected = classrooms[i];
+            Classroom actual = classroomDaoPostgres.findOne(i + 1);
+            assertTrue(equals(expected, actual));
+        }
+    }
 
-	@Test
-	public void classroomFindAllTest() throws Exception {
-		List<Classroom> classrooms = classroomDaoPostgres.findAll();
-		assertTrue(classrooms.size() == 5);
-		for (Classroom room : classrooms) {
-			assertTrue(exist(room.getId()));
-		}
-	}
+    @Test
+    public void classroomFindAllTest() throws Exception {
+        List<Classroom> classrooms = classroomDaoPostgres.findAll();
+        assertTrue(classrooms.size() == 5);
+        for (Classroom room : classrooms) {
+            assertTrue(exist(room.getId()));
+        }
+    }
 
-	@Test
-	public void classroomUpdateTest() throws Exception {
-		Classroom[] updates = { new Classroom(1, "106", "13", 30), new Classroom(2, "989", "23", 150),
-				new Classroom(3, "321", "23a", 320), new Classroom(4, "401", "24zxc", 100),
-				new Classroom(5, "3a05", "24", 30) };
-		for (int i = 0; i < updates.length; i++) {
-			classroomDaoPostgres.update(updates[i]);
-		}
-		String sql = "SELECT * FROM CLASSROOMS ORDER BY ID ASC;";
-		Classroom expected, actual;
-		try (Connection connection = ConnectionFactory.getConnection();
-				PreparedStatement statement = connection.prepareStatement(sql)) {
-			statement.execute();
-			try (ResultSet resultSet = statement.getResultSet()) {
-				for (int i = 0; i < updates.length; i++) {
-					resultSet.next();
-					expected = updates[i];
-					actual = new Classroom(resultSet.getInt("ID"), resultSet.getString("NUMBER"),
-							resultSet.getString("BUILDING"), resultSet.getInt("CAPACITY"));
-					assertTrue(compare(expected, actual));
-				}
-			}
-		} catch (SQLException e) {
-		}
-	}
+    @Test
+    public void classroomUpdateTest() throws Exception {
+        Classroom[] classrooms = { new Classroom(1, "106", "13", 30), new Classroom(2, "989", "23", 150),
+                new Classroom(3, "321", "23a", 320), new Classroom(4, "405", "24zxc", 100),
+                new Classroom(5, "305", "24", 35) };
+        for (int i = 0; i < classrooms.length; i++) {
+            classroomDaoPostgres.update(classrooms[i]);
+        }
+        assertEquals((String) H2handler.getField(TABLENAME, BUILDING, 1), "13");
+        assertEquals((int) H2handler.getField(TABLENAME, CAPACITY, 2), 150);
+        assertEquals((String) H2handler.getField(TABLENAME, BUILDING, 3), "23a");
+        assertEquals((String) H2handler.getField(TABLENAME, NUMBER, 4), "405");
+        assertEquals((int) H2handler.getField(TABLENAME, CAPACITY, 5), 35);
+    }
 
-	@Test(expected = DaoException.class)
-	public void classroomUpdateExceptionTest() throws Exception {
-		int notExistID = 1_000;
-		Classroom classroom = new Classroom();
-		classroom.setId(notExistID);
-		classroomDaoPostgres.update(classroom);
-	}
+    @Test(expected = DaoException.class)
+    public void classroomUpdateExceptionTest() throws Exception {
+        int notExistID = 1_000;
+        Classroom classroom = new Classroom();
+        classroom.setId(notExistID);
+        classroomDaoPostgres.update(classroom);
+    }
 
-	@Test
-	public void classroomDeleteTest() throws Exception {
-		for (int i = 1; i < 6; i++) {
-			classroomDaoPostgres.delete(i);
-			assertFalse(exist(i));
-		}
-	}
+    @Test
+    public void classroomDeleteTest() throws Exception {
+        for (int i = 1; i < 6; i++) {
+            classroomDaoPostgres.delete(i);
+            assertFalse(exist(i));
+        }
+    }
 
-	@Before
-	public void initialize() throws Exception {
-		h2handler.initialize();
-	}
+    @Before
+    public void initialize() throws Exception {
+        h2handler.createTable(SQL_CREATE_FILENAME);
+        h2handler.insertContent(SQL_INSERT_FILENAME);
+    }
 
-	private boolean exist(Integer id) throws DaoException {
-		return H2handler.exist(tablename, id);
-	}
+    private boolean exist(Integer id) throws DaoException {
+        return H2handler.exist(TABLENAME, id);
+    }
 
-	private boolean compare(Classroom expected, Classroom actual) {
-		if (expected.getId() == actual.getId() && expected.getNumber().equals(actual.getNumber())
-				&& expected.getBuilding().equals(actual.getBuilding())
-				&& expected.getCapacity() == actual.getCapacity()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+    private boolean equals(Classroom expected, Classroom actual) {
+        if (expected.getId() == actual.getId() && expected.getNumber().equals(actual.getNumber())
+                && expected.getBuilding().equals(actual.getBuilding())
+                && expected.getCapacity() == actual.getCapacity()) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/net/imshenik/university/H2handler.java
+++ src/test/java/net/imshenik/university/H2handler.java
@@ -12,90 +12,105 @@ import net.imshenik.university.dao.ConnectionFactory;
 import net.imshenik.university.dao.DaoException;
 
 public class H2handler {
-	private static H2handler instance;
-	String sqlCreateTable = "";
+    private static H2handler instance;
+    private String sqlCreateTable = "";
+    private String sqlInsertContent = "";
 
-	public static H2handler getInstance() {
-		if (instance == null) {
-			instance = new H2handler();
-		}
-		return instance;
-	}
+    private H2handler() {
+    }
 
-	public void initialize() throws Exception {
-		createTable("H2ClassroomsDropCreate.sql");
-		insertContent("H2ClassroomsInsert.sql");
-	}
+    public static H2handler getInstance() {
+        if (instance == null) {
+            instance = new H2handler();
+        }
+        return instance;
+    }
 
-	private void createTable(String sqlSource) throws Exception {
-		if (sqlCreateTable == "") {
-			try (FileReader fileReader = new FileReader(
-					new File(H2handler.class.getClassLoader().getResource("sql/" + sqlSource).getFile()))) {
-				int i = fileReader.read();
-				while (i != -1) {
-					sqlCreateTable += (char) i;
-					i = fileReader.read();
-				}
-			}
-		}
-		try (Connection connection = ConnectionFactory.getConnection(); Statement stmt = connection.createStatement()) {
-			stmt.execute(sqlCreateTable);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void createTable(String sqlSource) throws Exception {
+        if (sqlCreateTable == "") {
+            try (FileReader fileReader = new FileReader(
+                    new File(H2handler.class.getClassLoader().getResource("sql/" + sqlSource).getFile()))) {
+                int i = fileReader.read();
+                while (i != -1) {
+                    sqlCreateTable += (char) i;
+                    i = fileReader.read();
+                }
+            }
+        }
+        try (Connection connection = ConnectionFactory.getConnection(); Statement stmt = connection.createStatement()) {
+            stmt.execute(sqlCreateTable);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void insertContent(String sqlSource) {
-		String sql = "";
-		try (FileReader fileReader = new FileReader(
-				new File(H2handler.class.getClassLoader().getResource("sql/" + sqlSource).getFile()))) {
-			int i = fileReader.read();
-			while (i != -1) {
-				sql += (char) i;
-				i = fileReader.read();
-			}
-			Class.forName("org.h2.Driver");
-			try (Connection connection = ConnectionFactory.getConnection();
-					Statement stmt = connection.createStatement()) {
-				stmt.executeUpdate(sql);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void insertContent(String sqlSource) {
+        if (sqlInsertContent == "") {
+            try (FileReader fileReader = new FileReader(
+                    new File(H2handler.class.getClassLoader().getResource("sql/" + sqlSource).getFile()))) {
+                int i = fileReader.read();
+                while (i != -1) {
+                    sqlInsertContent += (char) i;
+                    i = fileReader.read();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try (Connection connection = ConnectionFactory.getConnection(); Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(sqlInsertContent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static int getNumberOfRows(String tablename) throws Exception {
-		int quantity = 0;
-		String sql = String.format("SELECT COUNT(*) FROM %s;", tablename);
-		try (Connection connection = ConnectionFactory.getConnection();
-				PreparedStatement statement = connection.prepareStatement(sql)) {
-			try (ResultSet resultSet = statement.executeQuery()) {
-				resultSet.next();
-				quantity = resultSet.getInt(1);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return quantity;
-	}
+    public static int getNumberOfRows(String tablename) throws Exception {
+        int quantity = 0;
+        String sql = String.format("SELECT COUNT(*) FROM %s;", tablename);
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                quantity = resultSet.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return quantity;
+    }
 
-	public static boolean exist(String tablename, Integer id) throws DaoException {
-		if (id == null) {
-			return false;
-		}
-		boolean found = false;
-		String sql = String.format("SELECT EXISTS(SELECT 1 FROM %s WHERE ID=%d);", tablename, id);
-		try (Connection connection = ConnectionFactory.getConnection();
-				PreparedStatement statement = connection.prepareStatement(sql)) {
-			try (ResultSet resultSet = statement.executeQuery()) {
-				resultSet.next();
-				if (resultSet.getBoolean(1) == true) {
-					found = true;
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return found;
-	}
+    public static boolean exist(String tablename, Integer id) throws DaoException {
+        if (id == null) {
+            return false;
+        }
+        boolean found = false;
+        String sql = String.format("SELECT EXISTS(SELECT 1 FROM %s WHERE ID=%d);", tablename, id);
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                if (resultSet.getBoolean(1) == true) {
+                    found = true;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return found;
+    }
+
+    public static Object getField(String table, String column, Integer id) throws DaoException {
+        String sql = String.format("SELECT %s FROM %s WHERE ID=%d;", column, table, id);
+        Object data = null;
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                data = resultSet.getObject(column);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
 }
