741b775edeff3595b31ba54e629e007c90da2300
aimshenik <a.imshenick@gmail.com>
2018-11-28 19:05:41 +0300

    DAO AND TESTS IN ONE FORMAT NOW + SOME REMARKS FROM SLACK FIXED

--- src/test/java/net/imshenik/university/ScheduleDaoPostgresTest.java
+++ src/test/java/net/imshenik/university/ScheduleDaoPostgresTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -63,15 +64,16 @@ public class ScheduleDaoPostgresTest {
         Classroom[] classrooms = { new Classroom(1, "106", "23", 30), new Classroom(2, "211", "23", 50),
                 new Classroom(3, "321", "23", 30), new Classroom(4, "401", "24", 100),
                 new Classroom(5, "305", "24", 30) };
-        LocalDateTime[] starts = { LocalDateTime.parse("2018-01-09T10:30:00"),
-                LocalDateTime.parse("2018-01-09T09:00:00"), LocalDateTime.parse("2018-01-09T12:00:00"),
+        LocalDateTime[] starts = { LocalDateTime.parse("2018-01-09T09:00:00"),
+                LocalDateTime.parse("2018-01-09T10:30:00"), LocalDateTime.parse("2018-01-09T12:00:00"),
                 LocalDateTime.parse("2018-01-09T14:00:00"), LocalDateTime.parse("2018-01-09T15:30:00"), };
         LocalDateTime[] ends = { LocalDateTime.parse("2018-01-09T10:20:00"), LocalDateTime.parse("2018-01-09T11:50:00"),
                 LocalDateTime.parse("2018-01-09T13:20:00"), LocalDateTime.parse("2018-01-09T15:20:00"),
                 LocalDateTime.parse("2018-01-09T16:50:00"), };
         Schedule[] schedules = new Schedule[5];
         for (int i = 0; i < schedules.length; i++) {
-            System.out.println(i);
+            schedules[i] = new Schedule();
+            schedules[i].setId(i + 1);
             schedules[i].setClassroom(classrooms[i]);
             schedules[i].setGroup(groups[i]);
             schedules[i].setTeacher(teachers[i]);
@@ -86,38 +88,71 @@ public class ScheduleDaoPostgresTest {
         }
     }
 
-//    @Test
-    public void ScheduleFindAllTest() throws Exception {
-        List<Schedule> Schedules = scheduleDaoPostgres.findAll();
-        assertTrue(Schedules.size() == 5);
-        for (Schedule Schedule : Schedules) {
-            assertTrue(exist(Schedule.getId()));
+    @Test
+    public void scheduleFindAllTest() throws Exception {
+        List<Schedule> schedules = scheduleDaoPostgres.findAll();
+        assertTrue(schedules.size() == 5);
+        for (Schedule schedule : schedules) {
+            assertTrue(exist(schedule.getId()));
         }
     }
 
-//    @Test
-    public void ScheduleUpdateTest() throws Exception {
-        Schedule[] Schedules = new Schedule[5];
-        for (int i = 0; i < Schedules.length; i++) {
-            scheduleDaoPostgres.update(Schedules[i]);
+    @Test
+    public void scheduleUpdateTest() throws Exception {
+        Teacher[] teachers = { new Teacher(1, "ANDREY FEDOROVICH", "SMIKOV", "AA-11111"),
+                new Teacher(2, "VIKTOR SERGEEVICH", "MOISEEV", "AA-22222"),
+                new Teacher(3, "DMITRY VASILYEVICH", "BEREZHNOY", "AA-33333"),
+                new Teacher(4, "BORIS LEONIDOVICH", "BOBRYSHEV", "AA-44444"),
+                new Teacher(5, "OLGA VALENTINOVNA", "TELITSYNA", "AA-55555") };
+        Subject[] subjects = { new Subject(1, "Heat engineering and heat transfer basics"),
+                new Subject(2, "Metrology, standardization and certification"),
+                new Subject(3, "Safety of vital functions"), new Subject(4, "Basics of construction"),
+                new Subject(5, "Foundry technology") };
+        Group[] groups = { new Group(1, "1-D-31"), new Group(2, "1-L-32"), new Group(3, "2-M-43"),
+                new Group(4, "3-F-64"), new Group(5, "3-SK-90") };
+        Classroom[] classrooms = { new Classroom(1, "106", "23", 30), new Classroom(2, "211", "23", 50),
+                new Classroom(3, "321", "23", 30), new Classroom(4, "401", "24", 100),
+                new Classroom(5, "305", "24", 30) };
+        LocalDateTime[] starts = { LocalDateTime.parse("2018-01-09T09:00:00"),
+                LocalDateTime.parse("2018-01-09T10:30:00"), LocalDateTime.parse("2018-01-09T12:00:00"),
+                LocalDateTime.parse("2018-01-09T14:00:00"), LocalDateTime.parse("2021-01-09T15:30:00") };
+        LocalDateTime[] ends = { LocalDateTime.parse("2018-01-09T10:20:00"), LocalDateTime.parse("2018-01-09T11:50:00"),
+                LocalDateTime.parse("2018-01-09T13:20:00"), LocalDateTime.parse("2018-01-09T15:20:00"),
+                LocalDateTime.parse("2021-01-09T16:50:00") };
+        Schedule[] schedules = new Schedule[5];
+        for (int i = 0; i < schedules.length; i++) {
+            schedules[i] = new Schedule();
+            schedules[i].setId(i + 1);
+            schedules[i].setClassroom(classrooms[i == 4 ? 0 : i + 1]);
+            schedules[i].setGroup(groups[i == 4 ? 0 : i + 1]);
+            schedules[i].setTeacher(teachers[i == 4 ? 0 : i + 1]);
+            schedules[i].setSubject(subjects[i == 4 ? 0 : i + 1]);
+            schedules[i].setStart(starts[i]);
+            schedules[i].setEnd(ends[i]);
         }
-//        assertEquals((int) H2handler.getField(TABLENAME, GROUP_ID, 1), 2);
-//        assertEquals((String) H2handler.getField(TABLENAME, LASTNAME, 2), "STEPANKIN");
-//        assertEquals((String) H2handler.getField(TABLENAME, FIRSTNAME, 3), "GEORG");
-//        assertEquals((int) H2handler.getField(TABLENAME, GROUP_ID, 4), 100);
-//        assertEquals((String) H2handler.getField(TABLENAME, LASTNAME, 5), "IVANOVA");
+        for (int i = 0; i < schedules.length; i++) {
+            scheduleDaoPostgres.update(schedules[i]);
+        }
+        assertEquals((int) H2handler.getField(TABLENAME, TEACHER_ID, 1), 2);
+        assertEquals((int) H2handler.getField(TABLENAME, SUBJECT_ID, 2), 3);
+        assertEquals((int) H2handler.getField(TABLENAME, GROUP_ID, 3), 4);
+        assertEquals((int) H2handler.getField(TABLENAME, CLASSROOM_ID, 4), 5);
+        assertTrue(((Timestamp) H2handler.getField(TABLENAME, START_TIME, 5)).toLocalDateTime()
+                .equals(LocalDateTime.parse("2021-01-09T15:30:00")));
+        assertTrue(((Timestamp) H2handler.getField(TABLENAME, END_TIME, 5)).toLocalDateTime()
+                .equals(LocalDateTime.parse("2021-01-09T16:50:00")));
     }
 
-//    @Test(expected = DaoException.class)
-    public void ScheduleUpdateExceptionTest() throws Exception {
+    @Test(expected = DaoException.class)
+    public void scheduleUpdateExceptionTest() throws Exception {
         int notExistID = 1_000;
-        Schedule Schedule = new Schedule();
-        Schedule.setId(notExistID);
-        scheduleDaoPostgres.update(Schedule);
+        Schedule schedule = new Schedule();
+        schedule.setId(notExistID);
+        scheduleDaoPostgres.update(schedule);
     }
 
-//    @Test
-    public void ScheduleDeleteTest() throws Exception {
+    @Test
+    public void scheduleDeleteTest() throws Exception {
         for (int i = 1; i < 6; i++) {
             scheduleDaoPostgres.delete(i);
             assertFalse(exist(i));
