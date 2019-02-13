84cd0f7c98f041ab5b35dfab3ef2c0d4b7ca3042
Andrey <a.imshenick@gmail.com>
2019-02-13 17:59:27 +0300

    lookup moved to separate method

--- src/main/java/net/imshenik/university/dao/ConnectionFactory.java
+++ src/main/java/net/imshenik/university/dao/ConnectionFactory.java
@@ -11,17 +11,28 @@ import org.apache.log4j.Logger;
 public class ConnectionFactory {
     private static final Logger log = Logger.getLogger(ConnectionFactory.class.getName());
     private static DataSource ds;
-    
+
     public static Connection getConnection() throws DaoException {
+        if (ds == null) {
+            lookup();
+        }
         Connection connection = null;
         try {
-            Context ctx = new InitialContext();
-            log.trace("Loading Datasource from Context");
-            ds = (DataSource) ctx.lookup("java:comp/env/jdbc/universitydb");
             connection = ds.getConnection();
-        } catch (NamingException | SQLException e) {
+        } catch (SQLException e) {
             throw new DaoException("Unable provide connection to database", e);
         }
         return connection;
+    }
+
+    private static void lookup() {
+        Context ctx;
+        try {
+            ctx = new InitialContext();
+            log.trace("Loading Datasource from Context");
+            ds = (DataSource) ctx.lookup("java:comp/env/jdbc/universitydb");
+        } catch (NamingException e) {
+            throw new DaoException("Unable provide lookup Datasource", e);
+        }
     }
 }
