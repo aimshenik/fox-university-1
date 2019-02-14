0a5c99be4c1f11c03e55202b52e4afd98e7089a5
Andrey <a.imshenick@gmail.com>
2019-02-14 17:06:22 +0300

    ConnectionFactory.java : ds inline initialization, removed redundand

--- src/main/java/net/imshenik/university/dao/ConnectionFactory.java
+++ src/main/java/net/imshenik/university/dao/ConnectionFactory.java
@@ -2,7 +2,6 @@ package net.imshenik.university.dao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -10,27 +9,20 @@ import org.apache.log4j.Logger;
 
 public class ConnectionFactory {
     private static final Logger log = Logger.getLogger(ConnectionFactory.class.getName());
-    private static DataSource ds;
+    private static DataSource ds = lookup();
 
     public static Connection getConnection() throws DaoException {
-        if (ds == null) {
-            lookup();
-        }
-        Connection connection = null;
         try {
-            connection = ds.getConnection();
+            return ds.getConnection();
         } catch (SQLException e) {
             throw new DaoException("Unable provide connection to database", e);
         }
-        return connection;
     }
 
-    private static void lookup() {
-        Context ctx;
+    private static DataSource lookup() {
         try {
-            ctx = new InitialContext();
             log.trace("Loading Datasource from Context");
-            ds = (DataSource) ctx.lookup("java:comp/env/jdbc/universitydb");
+            return (DataSource) new InitialContext().lookup("java:comp/env/jdbc/universitydb");
         } catch (NamingException e) {
             throw new DaoException("Unable provide lookup Datasource", e);
         }
